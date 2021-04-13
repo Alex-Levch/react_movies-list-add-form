ea83a5292ebfa94f11e31975f43b9cc057af4ff0
Alexander Levchenko <a.levchenko603@gmail.com>
2021-04-14 02:11:51 +0300

    Updates

---
+++ precache-manifest.60ca1280341c7ceeff3e1b1e2e381af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10f9b3ea5e2263e21a7cf2b70003d7f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34f2b7fef94caea689c8",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "9432f52d79820fc996fc",
+    "url": "./static/css/main.54a7b2c0.chunk.css"
+  },
+  {
+    "revision": "34f2b7fef94caea689c8",
+    "url": "./static/js/2.ac1aaba4.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.ac1aaba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9432f52d79820fc996fc",
+    "url": "./static/js/main.7b505431.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
